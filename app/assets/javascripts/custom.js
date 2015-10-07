49fe002641a1b3b4cb874fdb2bb586f1d444445c
alexa08 <alexaa0088@gmail.com>
2015-10-07 02:12:32 +0000

    Carousel selectors fix

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,88 +1,87 @@
-$(function(){
-    
+$(function() {
+
     "use strict";
     
-    var topoffset = 50; //variable for menu height
+    var topoffset = 50; //Variable for menu height
     var slideqty = $('#featured .item').length;
-    var wheight = $(window).height(); //get the height of the window
+    var wheight = $(window).height(); //Get the height of the window
     var randSlide = Math.floor(Math.random()*slideqty);
     
     $('#featured .item').eq(randSlide).addClass('active');
     
     
-    $('.fullheight').css('height', wheight); //set to window tallness
+    $('.fullheight').css('height', wheight); //Set to window tallness  
     
-    //Replace IMG inside carousels with a background image
+    
+    //rReplace IMG inside carousels with a background image
     $('#featured .item img').each(function() {
-       var imgSrc = $(this).attr('src');
-       $(this).parent().css({'background-image': 'url('+imgSrc+')'});
-       $(this).remove();
+        var imgSrc = $(this).attr('src');
+        $(this).parent().css({'background-image': 'url('+imgSrc+')'});
+        $(this).remove();
     });
-    
     
     //Adjust height of .fullheight elements on window resize
     $(window).resize(function() {
-        wheight = $(window).height(); //get the height of the window
-        $('.fullheight').css('height', wheight); //set to window tallness
+        wheight = $(window).height(); //Get the height of the window
+        $('.fullheight').css('height', wheight); //Set to window tallness  
     });
+    
+    
     
     //Activate Scrollspy
     $('body').scrollspy({
-       target: 'header .navbar',
-       offset: topoffset
+        target: 'header .navbar',
+        offset: topoffset
     });
     
-    //add inbody class
-    var hash = $(this).find('li.active a').attr('href')
+    // Add inbody class
+    var hash = $(this).find('li.active a').attr('href');
+    if(hash !== '#featured') {
+        $('header nav').addClass('inbody');
+    } else {
+        $('header nav').removeClass('inbody');
+    }
     
-        if(hash !== '#featured') {
-            $('header nav').addClass('inbody');
-        } else {
-            $('header nav').removeClass('inbody');
-        }
     
-    
-    //Add an inbody class to nav when scrollspy event fires
-    $('.navbar-fixed-top').on('activate.bs.scrollspy', function(){
-        var hash = $(this).find('li.active a').attr('href')
-    
-        if(hash !== '#featured') {
-            $('header nav').addClass('inbody');
-        } else {
-            $('header nav').removeClass('inbody');
-        }
+    // Add an inbody class to nav when scrollspy event fires
+    $('.navbar-fixed-top').on('activate.bs.scrollspy', function() {
+      var hash = $(this).find('li.active a').attr('href');
+      if(hash !== '#featured') {
+        $('header nav').addClass('inbody');
+      } else {
+        $('header nav').removeClass('inbody');
+      }
     });
     
     
-    //smooth scrolling
+    //Use smooth scrolling when clicking on navigation
     $('.navbar a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//,'') ===
-          this.pathname.replace(/^\//,'') && 
-          location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html,body').animate({
-              scrollTop: target.offset().top-topoffset+2
-            }, 1000);
-            return false;
-          } //target.length
-        } //click function
+      if (location.pathname.replace(/^\//,'') === 
+        this.pathname.replace(/^\//,'') && 
+        location.hostname === this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+             scrollTop: target.offset().top-topoffset+2
+          }, 500);
+          return false;
+        } //target.length
+      } //click function
     }); //smooth scrolling
-
     
     //Automatically generate carousel indicators
     for (var i=0; i < slideqty; i++) {
         var insertText = '<li data-target="#featured" data-slide-to="' + i + '"';
         if (i === randSlide) {
-            insertText += ' class="active" '; 
+          insertText += ' class="active" ';
         }
-        '></li>';
+        insertText += '></li>';
         $('#featured ol').append(insertText);
     }
     
     $('.carousel').carousel({
-        pause: false
+      pause: false
     });
 
 });
